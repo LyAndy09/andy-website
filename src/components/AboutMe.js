78c68ef0fb1f14c5705d17e75a6f4a3aa7075f4a
Andy Ly <LyAnd@oregonstate.edu>
2022-11-15 10:47:39 -0800

    added some more fun tidbits about me

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -6,7 +6,8 @@ function AboutMe() {
             <h2 className="about--title">About Me</h2>
             <p className="about--text">
                 Currently a 3rd year C.S. student at Oregon State University pursuing passion to be a full stack developer.
-                If I'm not fixing my code then you'll probably find me working on my cars, practicing olympic lifting, or watching funny corgi videos!
+                When coding, I'll be listening to r&b music and browsing StackOverflow.
+                If I'm not fixing my code then you'll find me working on my cars, practicing olympic lifting, or watching funny corgi videos!
             </p>
         </div>)
 }
